ac1506ade166b2a53205477ecac1c79501ff6557
Mohammad Halhuli <hlhwlymhmmd@gmail.com>
2023-05-29 16:15:57 +0300

    added

--- src/main/java/com/example/currencyconversion/CurrencyConversionApplication.java
+++ src/main/java/com/example/currencyconversion/CurrencyConversionApplication.java
@@ -14,8 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class CurrencyConversionApplication implements CommandLineRunner {
 
 
-    @Autowired
-    private CurrencyRepository currencyRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CurrencyConversionApplication.class, args);
     }
@@ -27,17 +26,5 @@ public class CurrencyConversionApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-      //  Currency currency = new Currency();
-//        currency.set_id(12321313);
-//        currency.setFrom("SAL");
-
-
-        // set other fields...
-
-       // currencyRepository.save(currency);
-       // System.out.println(currencyRepository.findAll());
-
-
-        System.out.println("Saved currency to MongoDB.");
     }
 }
